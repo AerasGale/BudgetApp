aeae2ce69c6f71912b57b712e1d757f81c6c4ce8
AerasGale <58614413+AerasGale@users.noreply.github.com>
2023-11-23 17:56:31 +0800

    fix: Stop the spinner from losing its place on configuration changes.
    -Also got rid of various unused imports and local variables to satisfy commit checks

--- app/src/main/java/com/example/budgetapp/fragments/add_transaction/AddTransactionFragment.java
+++ app/src/main/java/com/example/budgetapp/fragments/add_transaction/AddTransactionFragment.java
@@ -2,11 +2,11 @@ package com.example.budgetapp.fragments.add_transaction;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.util.Log;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,40 +22,38 @@ import com.example.budgetapp.databinding.FragmentAddTransactionBinding;
 import com.example.budgetapp.transaction.TransactionType;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 public class AddTransactionFragment extends Fragment {
-    private static final String TAG = "AddFragment";
     private static final String SPINNER_SELECTED_ITEM = "arg_selected_item";
     private AddViewModel addViewModel;
     private FragmentAddTransactionBinding binding;
-    private LiveData<List<String>> accountNames;
+    private Spinner spinnerAccountNames;
+    private boolean pendingReset = true;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentAddTransactionBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        Spinner accSelector = binding.spnAccount;
+        spinnerAccountNames = binding.spnAccount;
         Button addButton = binding.btnAddTransaction;
 
-        addViewModel = new ViewModelProvider(this.getActivity()).get(AddViewModel.class);
-        accountNames = addViewModel.getAllAccountNames();
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this.getContext(), android.R.layout.simple_spinner_item);
+        addViewModel = new ViewModelProvider(this.requireActivity()).get(AddViewModel.class);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this.requireContext(), android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        accSelector.setAdapter(adapter);
-        if(savedInstanceState!= null){
-            accSelector.setSelection(savedInstanceState.getInt(SPINNER_SELECTED_ITEM, 0));
-        }
-
+        spinnerAccountNames.setAdapter(adapter);
 
         // Listeners
         addViewModel.getAllAccountNames().observe(getViewLifecycleOwner(), strings -> {
             adapter.clear();
             adapter.addAll(strings);
             adapter.notifyDataSetChanged();
-            Log.d(TAG, "onChanged: Account names are " + strings);
+
+            if(savedInstanceState!= null && pendingReset){
+                spinnerAccountNames.setSelection(savedInstanceState.getInt(SPINNER_SELECTED_ITEM, 0));
+                pendingReset = false;
+            }
         });
 
         addButton.setOnClickListener(v -> {
@@ -73,15 +71,14 @@ public class AddTransactionFragment extends Fragment {
             } else {
                 return;
             }
-            addViewModel.createTransaction(accSelector.getSelectedItem().toString(), amount, transactionType);
+            addViewModel.createTransaction(spnAccount.getSelectedItem().toString(), amount, transactionType);
         });
-        accSelector.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spinnerAccountNames.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String selected = parent.getSelectedItem().toString();
-                if(selected!=null){
+                if(selected.trim().length()!=0){
                     Toast.makeText(parent.getContext(), "Selected " + selected, Toast.LENGTH_SHORT).show();
-
                 }
             }
 
@@ -92,5 +89,11 @@ public class AddTransactionFragment extends Fragment {
         });
 
         return root;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putInt(SPINNER_SELECTED_ITEM, spinnerAccountNames.getSelectedItemPosition());
+        super.onSaveInstanceState(outState);
     }
 }
